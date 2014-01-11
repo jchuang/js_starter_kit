aba45bf616bc994d96a5b947a0d696da59414da9
Janet Chuang <janet.chuang@gmail.com>
2014-01-10 23:32:42 -0500

    accepts user input when button is clicked

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-alert('Hey find me to know where to put the javascript');
+var max = 50;
+var userGuess;
+
+function getRandomInt( min, max ) {
+  return Math.floor(Math.random() * ( max - min + 1 ) + min );
+}
+
+var number = getRandomInt( 0, max );
+
+$( document ).ready(function() {
+  $( '#max' ).html( max );
+  $( '.guess' ).on( 'click', function() {
+    userGuess = $( 'input' ).val();
+    console.log( userGuess );
+  });
+});
+
+console.log( 'the number is ' + number );
+console.log( "look, it's more text!" );
+
+console.log( parseInt( '15', 10 ));
+console.log( typeof( parseInt( '15', 10 )));
+
+console.log( max === 50 );
