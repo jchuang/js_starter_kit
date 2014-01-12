44cf8a89176200ee1f47d2e91a2c7b217ff25e90
Janet Chuang <janet.chuang@gmail.com>
2014-01-11 19:26:22 -0500

    checks if guess is a number between 0 and max

--- js/app.js
+++ js/app.js
@@ -1,32 +1,42 @@
 var max = 50;
 var userGuess;
 
-function getRandomInt( min, max ) {
+var numberGuess;
+var allGuesses = [];
+
+var getRandomInt = function( min, max ) {
   return Math.floor(Math.random() * ( max - min + 1 ) + min );
 }
 
 var number = getRandomInt( 0, max );
+console.log( 'the number is ' + number );
+
+var convertGuess = function( input ) {
+  return parseInt( input, 10 );
+}
 
 var submitGuess = function() {
   userGuess = $( 'input' ).val();
   $( 'input' ).val( '' );
-  console.log( userGuess );
-};
+  numberGuess = convertGuess( userGuess );
+
+  if( numberGuess > max || !numberGuess ) {
+    console.log( 'not a valid guess!' );
+  } else {
+    console.log( 'valid guess, check against number' );
+  }
+
+  allGuesses.push( userGuess );
+  console.log( 'allGuesses = ' + allGuesses );
+}
 
 $( document ).ready( function() {
   $( '#max' ).html( max );
+
   $( '.guess' ).on( 'click', submitGuess );
   $( 'input' ).on( 'keypress', function( event ) {
     if( event.keyCode == 13 ) {
       $( '.guess' ).click();
     }
-  });
-});
-
-console.log( 'the number is ' + number );
-console.log( "look, it's more text!" );
-
-console.log( parseInt( '15', 10 ));
-console.log( typeof( parseInt( '15', 10 )));
-
-console.log( max === 50 );
+  })
+})
