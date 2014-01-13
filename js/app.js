288161d75ac03f30b068cb1af94ef91d9e54f081
Janet Chuang <janet.chuang@gmail.com>
2014-01-13 17:02:24 -0500

    Edits from Adam's code review.
    -use isNaN function to test for input that is not a number after parseInt
    -remove extra spaces immediately inside parens
    -use IDs in html for more specific jQuery selection
    -both clicking on button and pressing enter call submitGuess function

--- js/app.js
+++ js/app.js
@@ -4,57 +4,57 @@ var userGuess;
 var numberGuess;
 var allGuesses = [];
 
-var getRandomInt = function( min, max ) {
-  return Math.floor(Math.random() * ( max - min + 1 ) + min );
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-var number = getRandomInt( 0, max );
-console.log( 'the number is ' + number );
+var number = getRandomInt(0, max);
 
-var convertGuess = function( input ) {
-  return parseInt( input, 10 );
+var convertGuess = function(input) {
+  return parseInt(input, 10);
 }
 
 var submitGuess = function() {
-  userGuess = $( 'input' ).val();
-  $( 'input' ).val( '' );
-  numberGuess = convertGuess( userGuess );
 
-  if ( numberGuess > max || numberGuess < 0 || !numberGuess ) {
-    $( '.alert' ).show();
-    $( '.alert' ).html( '<p>Oops! Please enter a number between 0 and ' + max + '.</p>' );
+  userGuess = $('#guess_value').val();
+  $('#guess_value').val('');
+  numberGuess = convertGuess(userGuess);
+
+  if (isNaN(numberGuess) || numberGuess > max || numberGuess < 0) {
+    $('.alert').show();
+    $('.alert').html('<p>Oops! Please enter a number between 0 and ' + max + '.</p>');
 
   } else {
-    allGuesses.push( userGuess );
-    $( '.alert' ).hide();
+    allGuesses.push(userGuess);
+    $('.alert').hide();
 
-    if ( numberGuess > number ) {
-      $( '.history' ).append( '<p class="pastguess">' + numberGuess + ' is too high, please guess again.</p>' );
-    } else if ( numberGuess < number ) {
-      $( '.history' ).append( '<p class="pastguess">' + numberGuess + ' is too low, please guess again.</p>' );
+    if (numberGuess > number) {
+      $('.history').append('<p class="pastguess">' + numberGuess + ' is too high, please guess again.</p>');
+    } else if (numberGuess < number) {
+      $('.history').append('<p class="pastguess">' + numberGuess + ' is too low, please guess again.</p>');
     } else {
-      $( '.history' ).append( '<p class="pastguess">Congratulations, ' + number + ' is correct!<br />You have guessed the number in ' +
-        allGuesses.length + ' guesses.</p>' );
-      $( '.success' ).show();
-      $( '.guess' ).hide();
-      $( 'input' ).prop( 'disabled', true );
+      $('.history').append('<p class="pastguess">Congratulations, ' + number + ' is correct!<br />You have guessed the number in ' +
+        allGuesses.length + ' guesses.</p>');
+      $('.success').show();
+      $('.guess').hide();
+      $('input').prop('disabled', true);
     }
   }
 }
 
 $( document ).ready( function() {
-  $( '#max' ).html( max );
-  $( '.alert' ).hide();
-  $( '.success' ).hide();
+  $('#max').html(max);
+  $('.alert').hide();
+  $('.success').hide();
 
-  $( '.guess' ).on( 'click', submitGuess );
-  $( 'input' ).on( 'keypress', function( event ) {
-    if( event.keyCode == 13 ) {
-      $( '.guess' ).click();
+  $('.guess').on('click', submitGuess);
+  $('#guess_value').on('keypress', function(event) {
+    if(event.keyCode == 13) {
+      submitGuess();
     }
   })
-  $( '.success' ).on( 'click', function() {
+  $('#new_game').on('click', function() {
     location.reload();
-    $( 'input' ).prop( 'disabled', false );
+    $('#guess_value').prop('disabled', false);
   })
 })
